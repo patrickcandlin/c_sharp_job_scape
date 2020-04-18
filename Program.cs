8abd1e642fd91e87d65621baaf10a8650768c5bf
“Patrick <pctennislessons@gmail.com>
2020-04-18 15:59:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace jobScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://denver.craigslist.org/search/bia?query=kids";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+
+            Console.WriteLine(html.Result);
+
+            Console.ReadLine();
+        }
+    }
+}
